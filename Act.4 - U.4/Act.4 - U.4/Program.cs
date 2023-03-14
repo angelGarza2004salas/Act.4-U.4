ddabbb6ddfadbdda3c35e68c2ff117affb25db93
angelGarza2004salas <125837002+angelGarza2004salas@users.noreply.github.com>
2023-03-14 13:49:43 -0600

    Add files via upload

---
+++ Act.4 - U.4/Act.4 - U.4/Program.cs
@@ -0,0 +1,32 @@
+﻿interface mAnimal
+{
+    void Sonido();
+}
+
+class Perro : mAnimal
+{
+    public void Sonido()
+    {
+        Console.WriteLine("Wuau wuau!");
+    }
+}
+
+class Gato : mAnimal
+{
+    public void Sonido()
+    {
+        Console.WriteLine("Miao miao!");
+    }
+}
+
+class Principal
+{
+    static void Main(string[] args)
+    {
+        mAnimal miPerro = new Perro();
+        mAnimal miGato = new Gato();
+
+        miPerro.Sonido();
+        miGato.Sonido();
+    }
+}
